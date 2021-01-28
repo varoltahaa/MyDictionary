20812431d100f2f10d822d2c94ae46c2c440943f
Taha <64753605+varoltahaa@users.noreply.github.com>
2021-01-28 15:55:35 +0300

    Add project files.

---
+++ MyDictionary/DictionaryManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class DictionaryManager<T, D>
+    {
+        T[] keys;
+        D[] values;
+
+        T[] tempArrayKeys;
+        D[] tempArrayValues;
+        
+        public DictionaryManager()
+        {
+            keys = new T[0];
+            values = new D[0];
+        }
+
+        public void Add(T key, D value)
+        {
+            tempArrayKeys = keys;
+            tempArrayValues = values;
+
+            keys = new T[keys.Length + 1];
+            values = new D[values.Length + 1];
+
+            for (int i = 0; i <tempArrayKeys.Length; i++)
+            {
+                keys[i] = tempArrayKeys[i];
+            }
+            for (int a = 0; a < tempArrayValues.Length; a++)
+            {
+                values[a] = tempArrayValues[a];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+
+        }
+        
+        public void ShowAll()
+        {
+            for (int i = 0; i <keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " = " + values[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictionaryManager<int,string> dictionary = new DictionaryManager<int,string>();
+            dictionary.Add(20,"Taha");
+            dictionary.Add(20,"Ender");
+            dictionary.ShowAll();
+
+        }
+    }
+}
